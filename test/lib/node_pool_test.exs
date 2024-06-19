44379a11b7da43eba13bb1a918870ecfbdf2670c
Chad Fennell <libsys@gmail.com>
2024-06-18 17:19:50 -0700

    Resolve timing issues
    There is probably a better way to handle this but our set_unhealthy/2
    function relies on Process.send_after to put a node back into rotation
    after a given timeout. And because tests within the same module are
    randomized, this means one set_unhealthy function call can alter
    the outcome of a different test. So, as a stopgap, I have adjuseted
    the timing of the set_unhealthy hooks to keep them from stepping
    on each other.

--- test/lib/node_pool_test.exs
+++ test/lib/node_pool_test.exs
@@ -44,12 +44,12 @@ defmodule TypesenseEx.NodePoolTest do
     test "set_unhealthy/2" do
       %{host: "localhost", port: "8109", protocol: "http"} = node = NodePool.next_node()
 
-      NodePool.set_unhealthy(node, 10)
+      NodePool.set_unhealthy(node, 1)
 
       assert %Node{host: "localhost", port: "8108", protocol: "http"} =
                node_two = NodePool.next_node()
 
-      NodePool.set_unhealthy(node_two, 10)
+      NodePool.set_unhealthy(node_two, 1)
 
       assert {:error, :no_healthy_nodes_available} = NodePool.next_node()
     end
@@ -87,13 +87,13 @@ defmodule TypesenseEx.NodePoolTest do
     end
 
     test "set_unhealthy/2" do
-      assert %{
+      assert %Node{
                host: "localhost",
                port: "8110",
                protocol: "http"
              } = nearest_node = NodePool.next_node()
 
-      NodePool.set_unhealthy(nearest_node, 5)
+      NodePool.set_unhealthy(nearest_node, 1)
 
       assert %Node{host: "localhost", port: "8109", protocol: "http"} =
                node_8109 =
@@ -103,7 +103,13 @@ defmodule TypesenseEx.NodePoolTest do
 
       assert {:error, :no_healthy_nodes_available} = NodePool.next_node()
 
-      Process.sleep(20)
+      Process.sleep(10)
+
+      assert %Node{
+               host: "localhost",
+               port: "8110",
+               protocol: "http"
+             } = NodePool.next_node()
 
       assert %Node{
                host: "localhost",
@@ -116,7 +122,7 @@ defmodule TypesenseEx.NodePoolTest do
   describe "with a nearest node and no nodes" do
     setup do
       nodes_configs = %{
-        nearest_node: %{host: "localhost", port: "8108", protocol: "http"},
+        nearest_node: %{host: "localhost", port: "8177", protocol: "http"},
         nodes: []
       }
 
@@ -131,22 +137,26 @@ defmodule TypesenseEx.NodePoolTest do
     test "next_node/0" do
       assert %Node{
                host: "localhost",
-               port: "8108",
+               port: "8177",
                protocol: "http"
              } = NodePool.next_node()
 
       assert %Node{
                host: "localhost",
-               port: "8108",
+               port: "8177",
                protocol: "http"
              } = NodePool.next_node()
-
     end
 
     test "set_unhealthy/2" do
-      %TypesenseEx.Node{host: "localhost", port: "8108", protocol: "http"} = node = NodePool.next_node()
+      %TypesenseEx.Node{host: "localhost", port: "8177", protocol: "http"} =
+        node = NodePool.next_node()
 
-      NodePool.set_unhealthy(node, 10)
+      # These tests run in a random order
+      # So, we set this to a long timeout
+      # so that Process.send_after doesn't
+      # pollute another test's ETS table
+      NodePool.set_unhealthy(node, 100)
 
       assert {:error, :no_healthy_nodes_available} = NodePool.next_node()
     end
